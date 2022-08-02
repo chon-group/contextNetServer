fe46980ca1af8eec847f96f994eb90b442f9457f
Prof Nilson Mori <nilsonmori@yahoo.com>
2022-08-02 12:44:48 -0300

    Install script

--- bin/chonosSkynet.sh
+++ bin/chonosSkynet.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
+SKYNET_HOME="/opt/chonos-skynet"
+
 while true
 do
 	echo "Iniciando contexNet"
-	/usr/bin/java -jar /opt/Robotic-agentCloud/OpenSplice/contextnet-2.7.jar 127.0.0.1 5500 OpenSplice &
+	/usr/bin/java -jar $SKYNET_HOME/libs/contextNet/contextnet-2.7.jar 127.0.0.1 5500 OpenSplice &
 	pid=$!
 	echo $pid
 	sleep 1800
--- bin/install.sh
+++ bin/install.sh
@@ -6,22 +6,7 @@ ln -s $SKYNET_HOME/deps/debian10-dpkgs/*.deb /var/cache/apt/archives/
 apt install /var/cache/apt/archives/adoptium-ca-certificates_1.0.0-1_all.deb -y
 apt install /var/cache/apt/archives/temurin-8-jdk_8.0.332.0.0+9-1_amd64.deb -y
 
-
-echo "Adicionando Link Simbólico para o OpenSplice................."
-ln -s $LOCAL$OPENSPLICEGit $OPENSPLICELOCAL'/HDE'
-
-echo "Alterando arquivo ospl.xml para o distribuído pela ContextNet"
-mv /opt/OpenSplice/HDE/x86_64.linux/etc/config/ospl.xml /opt/OpenSplice/HDE/x86_64.linux/etc/config/ospl.xml.OLD
-cp dependences/ospl.xml /opt/OpenSplice/HDE/x86_64.linux/etc/config/ospl.xml
-
-echo "Copiando variáveis de ambiente ............................."
-cp dependences/opensplice.sh /etc/profile.d/opensplice.sh
-
-echo "Habilitando script de inicialização ......................."
-ln -s /opt/Robotic-agentCloud/OpenSplice/contextNet.sh /usr/bin
-
-echo "Reinicie o sistema"
-
-echo ""
-
-echo "após reiniciar execute: contextNet.sh"
+#Instalação do OpenSplice
+ln -s $SKYNET_HOME/libs/OpenSplice /opt/OpenSplice
+ln -s $SKYNET_HOME/libs/OpenSplice/opensplice.sh /etc/profile.d/
+ln -s $SKYNET_HOME/bin/chonosSkynet.sh /usr/bin/chonosSkynet
