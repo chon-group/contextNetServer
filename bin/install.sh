4a4687a1bd7e9a791e9fcc08198c1a20d8b26f44
Prof Nilson Mori <nilsonmori@yahoo.com>
2022-08-02 12:34:26 -0300

    contextNet ospl.xml

--- bin/install.sh
+++ bin/install.sh
@@ -1,20 +1,11 @@
 #! /bin/sh
+SKYNET_HOME="/opt/chonos-skynet"
 
-echo "Configurando o Ambiente........................................"
-LOCAL=`pwd`
-OPENSPLICEGit='/OpenSplice/HDE'
-OPENSPLICELOCAL='/opt/OpenSplice'
+# Intalação do Java JDK 8
+ln -s $SKYNET_HOME/deps/debian10-dpkgs/*.deb /var/cache/apt/archives/
+apt install /var/cache/apt/archives/adoptium-ca-certificates_1.0.0-1_all.deb -y
+apt install /var/cache/apt/archives/temurin-8-jdk_8.0.332.0.0+9-1_amd64.deb -y
 
-# Intalando Dependências do Java JDK 8 (somente para debian 10)
-cat dependences/debian10-packages.tar.gz/* > /tmp/debian10-packages.tar.gz
-tar zxvf /tmp/debian10-packages.tar.gz -C /tmp/
-mv /tmp/debian10-dpkgs/* /var/cache/apt/archives/
-apt install /var/cache/apt/archives/adoptium-ca-certificates_1.0.0-1_all.deb 
-apt install /var/cache/apt/archives/temurin-8-jdk_8.0.332.0.0+9-1_amd64.deb
-
-
-echo "Criando diretório no /opt para o OpenSplice..................."
-mkdir -p $OPENSPLICELOCAL
 
 echo "Adicionando Link Simbólico para o OpenSplice................."
 ln -s $LOCAL$OPENSPLICEGit $OPENSPLICELOCAL'/HDE'
@@ -34,9 +25,3 @@ echo "Reinicie o sistema"
 echo ""
 
 echo "após reiniciar execute: contextNet.sh"
-
-
-
-
-
-
