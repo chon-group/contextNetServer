4762e2d9a8f3bb6046cc2df4fc0d4c61c6af89ac
Prof Nilson Mori <nilsonmori@yahoo.com>
2022-08-03 00:15:53 -0300

    default port 3273

--- bin/chonosSkynet.sh
+++ bin/chonosSkynet.sh
@@ -4,7 +4,7 @@ SKYNET_HOME="/opt/chonos-skynet"
 while true
 do
 	echo "Iniciando contexNet"
-	/usr/bin/java -jar $SKYNET_HOME/libs/OpenSplice/contextnet-2.7.jar 127.0.0.1 2065 OpenSplice &
+	/usr/bin/java -jar $SKYNET_HOME/libs/OpenSplice/contextnet-2.7.jar 127.0.0.1 3273 OpenSplice &
 	pid=$!
 	echo $pid
 	sleep 1800
