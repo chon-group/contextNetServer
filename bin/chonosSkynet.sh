ec6bb4b613bc8c74340cd2c4eba3535a4d3ec992
Nilson Lazarin <32855001+nilsonmori@users.noreply.github.com>
2023-08-16 16:01:46 -0300

    Update chonosSkynet.sh

--- bin/chonosSkynet.sh
+++ bin/chonosSkynet.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 SKYNET_HOME="/opt/chonos-skynet"
 
-while true
-do
+#while true
+#do
 	echo "Iniciando contexNet"
 	/usr/bin/java -jar $SKYNET_HOME/libs/OpenSplice/contextnet-2.7.jar 127.0.0.1 5500 OpenSplice &
-	pid=$!
-	echo $pid
-	sleep 1800
-	echo "Encerrando contexNet"
-	kill $pid
-done
+#	pid=$!
+#	echo $pid
+#	sleep 1800
+#	echo "Encerrando contexNet"
+#	kill $pid
+#done
