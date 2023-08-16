ad6eebe9425efd1a2a3301a3e9060ec5c4924638
Nilson Lazarin <32855001+nilsonmori@users.noreply.github.com>
2023-08-16 20:30:23 -0300

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -12,9 +12,9 @@ ln -s $SKYNET_HOME/libs/OpenSplice/opensplice.sh /etc/profile.d/
 ln -s $SKYNET_HOME/bin/chonosSkynet.sh /usr/bin/chonosSkynet
 
 # Modify the getty service file to enable autologin for root
-cp /lib/systemd/system/getty@.service /etc/systemd/system/getty@.service.backup
-sed -i 's|ExecStart=-/sbin/agetty |ExecStart=-/sbin/agetty --autologin root |' /lib/systemd/system/getty@.service
-sed -i 's|^ExecStart=-/sbin/agetty |ExecStartPre=/bin/login -f root\nExecStart=-/sbin/agetty |' /lib/systemd/system/getty@.service
+#cp /lib/systemd/system/getty@.service /etc/systemd/system/getty@.service.backup
+#sed -i 's|ExecStart=-/sbin/agetty |ExecStart=-/sbin/agetty --autologin root |' /lib/systemd/system/getty@.service
+#sed -i 's|^ExecStart=-/sbin/agetty |ExecStartPre=/bin/login -f root\nExecStart=-/sbin/agetty |' /lib/systemd/system/getty@.service
 
 echo "clear" >> /root/.profile
 echo 'echo "Waiting for DHCP Configuration"' >> /root/.profile
