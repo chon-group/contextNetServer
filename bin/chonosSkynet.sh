5fcd3ee856655f239a257c03b9bdb349899a0232
Nilson Lazarin <32855001+nilsonmori@users.noreply.github.com>
2023-08-17 09:35:38 -0300

    Update chonosSkynet.sh

--- bin/chonosSkynet.sh
+++ bin/chonosSkynet.sh
@@ -1,13 +1,20 @@
 #! /bin/sh
 SKYNET_HOME="/opt/contextNetServer"
 
-#while true
-#do
-	echo "Iniciando contexNet"
-	/usr/bin/java -jar $SKYNET_HOME/libs/OpenSplice/contextnet-2.7.jar 127.0.0.1 5500 OpenSplice &
-#	pid=$!
-#	echo $pid
-#	sleep 1800
-#	echo "Encerrando contexNet"
-#	kill $pid
-#done
+while true
+do
+	echo "Starting ContexNetServer"
+	ipv4=$(hostname -I | awk '{print $1}')
+	if [[ $ipv4 =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+     		/usr/bin/java -jar $SKYNET_HOME/libs/OpenSplice/contextnet-2.7.jar "$ipv4" 5500 OpenSplice &
+		pid=$!
+		echo $pid
+		sleep 600
+		echo "Restarting ContextNet..."
+		kill $pid
+	else
+	    echo "Isn't possible to get a IPv4. Restarting in 10 seconds..."
+	    sleep 10
+	    reboot
+	fi
+done
